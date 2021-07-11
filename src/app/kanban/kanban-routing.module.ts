4cbdc9e8148bf0f164c7f589fa6736daa1c8f7fe
gabriel <gabyk800@gmail.com>
2021-07-11 11:47:22 +0300

    implemented kanban board

---
+++ src/app/kanban/kanban-routing.module.ts
@@ -0,0 +1,11 @@
+import { BoardListComponent } from './board-list/board-list.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [{ path: '', component: BoardListComponent }];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class KanbanRoutingModule {}
